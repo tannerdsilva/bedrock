37ff370a5bcd479cfb8fe5f3d4dc44f700a67b12
Tanner Silva <t@tannersilva.com>
2023-02-02 09:58:13 -0600

    Delta is now fully public

--- Sources/bedrock/Delta.swift
+++ Sources/bedrock/Delta.swift
@@ -1,25 +1,27 @@
 /// The Delta struct makes it easy to compare additions or subtractions of a list of Hashable objects.
 public struct Delta<T> where T:Hashable {
 	/// the Type that is being compared
-	typealias DeltaType = T
+	public typealias DeltaType = T
 	
 	/// the hashed entities that exist in the starting dataset
-	let start:Set<DeltaType>
+	public let start:Set<DeltaType>
 	
 	/// hashed entities that are exclusive to the starting dataset
 	/// - items found in this set were not found in the ending dataset
-	let exclusiveStart:Set<DeltaType>
+	public let exclusiveStart:Set<DeltaType>
+	
 	/// hashed entities that are common to both the starting and ending dataset
-	let common:Set<DeltaType>
+	public let common:Set<DeltaType>
+	
 	/// hashed entities that are exclusive to the ending dataset
 	/// - items found in this set were not found in the beginning dataset
-	let end:Set<DeltaType>
+	public let end:Set<DeltaType>
 	
 	/// the hashed entities that exist in the ending dataset
-	let exclusiveEnd:Set<DeltaType>
+	public let exclusiveEnd:Set<DeltaType>
 	
 	/// Initialize a Delta struct based on two collections of the same Type.
-	init<S>(start startSeq:S, end endSeq:S) where S:Sequence, S.Element == T {
+	public init<S>(start startSeq:S, end endSeq:S) where S:Sequence, S.Element == T {
 		let start = Set(startSeq)
 		let end = Set(endSeq)
 		let differing = start.symmetricDifference(end)
--- Tests/bedrockTests/bedrockTests.swift
+++ Tests/bedrockTests/bedrockTests.swift
@@ -1,11 +1,34 @@
 import XCTest
 @testable import bedrock
+@testable import cbedrock
 
-final class bedrockTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(bedrock().text, "Hello, World!")
-    }
+final class bedrockTests:XCTestCase {
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(bedrock().text, "Hello, World!")
+//    }
+
+	func testLineParser() {
+		let lineTestData = [Data("this is a test of the first line".utf8), Data("".utf8), Data("this is a test of the third line".utf8), Data("line4".utf8)];
+		var patternData = Data()
+		patternData.append(10)
+		let lineDataMerged = Data(lineTestData.joined(separator:patternData))
+		var buildThings = [Data]()
+		patternData.withUnsafeMutableBytes({ patternIn in
+			var myLP = lp_init(patternIn.baseAddress!.bindMemory(to: UInt8.self, capacity:1), 1);
+			lineDataMerged.withUnsafeBytes({ dataIn in
+				lp_intake(&myLP, dataIn.baseAddress!.bindMemory(to:UInt8.self, capacity:dataIn.count), dataIn.count) { bytes, size in
+					let getData = Data(bytes:bytes, count:size)
+					buildThings.append(getData)
+				}
+			})
+			lp_close(&myLP) { bytes, size in
+				let getData = Data(bytes:bytes, count:size)
+				buildThings.append(getData)
+			};
+		})
+		XCTAssert(lineTestData == buildThings)
+	}
 }
