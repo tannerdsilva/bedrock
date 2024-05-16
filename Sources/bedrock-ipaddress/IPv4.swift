0de88596b0658d1516d123add396b6613d6ed019
tannerdsilva <t@tannersilva.com>
2024-05-15 20:19:47 -0500

    developed ipaddress

--- Sources/bedrock-ipaddress/IPv4.swift
+++ Sources/bedrock-ipaddress/IPv4.swift
@@ -3,7 +3,7 @@ import QuickLMDB
 
 @RAW_staticbuff(bytes:4)
 @MDB_comparable()
-public struct AddressV4:RAW_comparable_fixed {
+public struct AddressV4:RAW_comparable_fixed, Equatable, Comparable {
 	public init?(_ address:String) {
 		let parts = address.split(separator:".")
 		guard parts.count == 4 else {
@@ -23,6 +23,39 @@ public struct AddressV4:RAW_comparable_fixed {
 			return Self(RAW_staticbuff:$0.baseAddress!)
 		}
 	}
+
+	public init?(netmaskPrefix:UInt8) {
+		guard netmaskPrefix <= 32 else {
+			return nil
+		}
+		var bytes = [UInt8](repeating:0, count:4)
+		let fullBytes = Int(netmaskPrefix / 8)
+		let extraBits = Int(netmaskPrefix % 8)
+
+		// Fill full byte sections of the netmask
+		for i in 0..<fullBytes {
+			bytes[i] = 0xFF
+		}
+
+		// Set the remaining bits in the next byte
+		if extraBits > 0 {
+			bytes[fullBytes] = 0xFF << (8 - extraBits)
+		}
+
+		self.init(RAW_staticbuff:&bytes)
+	}
+
+	public func masked(by netmask:AddressV4) -> AddressV4 {
+		return self.RAW_access_staticbuff { addressBytes -> AddressV4 in
+			return netmask.RAW_access_staticbuff { maskBytes -> AddressV4 in
+				let addressPtr = addressBytes.assumingMemoryBound(to:UInt8.self)
+				let maskPtr = maskBytes.assumingMemoryBound(to:UInt8.self)
+				return withUnsafePointer(to:(addressPtr[0] & maskPtr[0], addressPtr[1] & maskPtr[1], addressPtr[2] & maskPtr[2], addressPtr[3] & maskPtr[3])) {
+					return AddressV4(RAW_staticbuff:$0)
+				}
+			}
+		}
+	}
 }
 
 extension String {
@@ -31,5 +64,63 @@ extension String {
 			let ptr = $0.assumingMemoryBound(to:AddressV4.RAW_staticbuff_storetype.self)
 			return "\(ptr.pointee.0).\(ptr.pointee.1).\(ptr.pointee.2).\(ptr.pointee.3)"
 		})
+	}
+}
+
+public typealias RangeV4 = ClosedRange<AddressV4>
+extension RangeV4 {
+	public init?(_ rangeString:String) {
+		let parts = rangeString.split(separator:"-", omittingEmptySubsequences:true).map(String.init)
+		guard parts.count == 2, let lower = AddressV4(parts[0]), let upper = AddressV4(parts[1]), lower <= upper else {
+			return nil
+		}
+		self = lower...upper
+	}
+
+	public init(address:AddressV4, netmask:AddressV4) {
+		var lowerBytes = [UInt8](repeating:0, count:4)
+		var upperBytes = [UInt8](repeating:0, count:4)
+		withUnsafePointer(to:address) { address in
+			withUnsafePointer(to:netmask) { netmask in
+				for i in 0..<4 {
+					lowerBytes[i] = address.withMemoryRebound(to:UInt8.self, capacity:4) { $0[i] & netmask.withMemoryRebound(to:UInt8.self, capacity:4) { $0[i] } }
+					upperBytes[i] = address.withMemoryRebound(to:UInt8.self, capacity:4) { $0[i] | ~netmask.withMemoryRebound(to:UInt8.self, capacity:4) { $0[i] } }
+				}
+			}
+		}
+		self = AddressV4(RAW_staticbuff:&lowerBytes)...AddressV4(RAW_staticbuff:&upperBytes)
+	}
+}
+
+public struct NetworkV4 {
+	public let address:AddressV4
+	public let prefix:UInt8
+	
+	public let range:RangeV4
+	
+	public init?(_ cidr:String) {
+		let parts = cidr.split(separator:"/").map(String.init)
+		guard parts.count == 2,
+			let address = AddressV4(parts[0]),
+			let prefix = UInt8(parts[1]),
+			prefix <= 32 else {
+			return nil
+		}
+		
+		self.address = address
+		self.prefix = prefix
+		let hasNetmaskPrefix = AddressV4(netmaskPrefix:prefix)
+		guard hasNetmaskPrefix != nil else {
+			return nil
+		}
+		self.range = RangeV4(address:address, netmask:hasNetmaskPrefix!)
+	}
+	
+	public func contains(_ address:AddressV4) -> Bool {
+		return range.contains(address)
+	}
+	
+	public func overlaps(with network:NetworkV4) -> Bool {
+		return range.overlaps(network.range)
 	}
 }
--- Sources/bedrock-ipaddress/IPv6.swift
+++ Sources/bedrock-ipaddress/IPv6.swift
@@ -3,58 +3,85 @@ import QuickLMDB
 
 @RAW_staticbuff(bytes:16)
 @MDB_comparable()
-public struct AddressV6:RAW_comparable_fixed {
+public struct AddressV6:RAW_comparable_fixed, Equatable, Comparable {
 	public init?(_ address:String) {
-		let segments = address.split(separator: ":", omittingEmptySubsequences: false)
-        var bytes = [UInt8]()
-        var compressionIndex: Int? = nil
+		let segments = address.split(separator:":", omittingEmptySubsequences:false)
+		var bytes = [UInt8]()
+		var compressionIndex: Int? = nil
 
-        // Count actual non-empty segments and identify compression
-        var actualSegmentCount = 0
-        for (index, segment) in segments.enumerated() {
-            if segment.isEmpty {
-                if compressionIndex == nil {
-                    compressionIndex = index
-                } else {
-                    // Multiple "::" found, which is invalid
-                    return nil
-                }
-            } else {
-                actualSegmentCount += 1
-            }
-        }
+		// coubt non-empty segments and identify compression index
+		var actualSegmentCount = 0
+		for (index, segment) in segments.enumerated() {
+			if segment.isEmpty {
+				if compressionIndex == nil {
+					compressionIndex = index
+				} else {
+					return nil
+				}
+			} else {
+				actualSegmentCount += 1
+			}
+		}
 
-        // Calculate the number of segments to be filled with zero
-        var segmentsToFill = 8 - actualSegmentCount
-        if compressionIndex != nil {
-            segmentsToFill += 1 // Include the "::" compression in the count
-        }
+		// calculate the number of segments to be filled with zero
+		var segmentsToFill = 8 - actualSegmentCount
+		if compressionIndex != nil {
+			segmentsToFill += 1 // Include the "::" compression in the count
+		}
 
-        // Parse and fill segments
-        for (index, segment) in segments.enumerated() {
-            if segment.isEmpty {
-                if index == compressionIndex { // Fill with zeros if it's the compression index
-                    for _ in 0..<segmentsToFill {
-                        bytes.append(0)
-                        bytes.append(0)
-                    }
-                }
-            } else {
-                guard let segmentValue = UInt16(segment, radix: 16) else {
-                    return nil // Invalid hexadecimal segment
-                }
-                bytes.append(UInt8(segmentValue >> 8))
-                bytes.append(UInt8(segmentValue & 0xFF))
-            }
-        }
+		// parse and fill segments
+		for (index, segment) in segments.enumerated() {
+			if segment.isEmpty {
+				if index == compressionIndex {
+					// fill with the correct number of zeros if it's the compression index
+					for _ in 0..<segmentsToFill {
+						bytes.append(0)
+						bytes.append(0)
+					}
+				}
+			} else {
+				let segmentValue = UInt16(segment, radix: 16)
+				guard segmentValue != nil else {
+					return nil // invalid hexadecimal segment
+				}
+				bytes.append(UInt8(segmentValue! >> 8))
+				bytes.append(UInt8(segmentValue! & 0xFF))
+			}
+		}
 
-        // Ensure the total bytes count is exactly 16
-        guard bytes.count == 16 else {
-            return nil
-        }
+		// Ensure the total bytes count is exactly 16
+		guard bytes.count == 16 else {
+			return nil
+		}
 
-		self = bytes.RAW_access {
-			return Self(RAW_staticbuff:$0.baseAddress!)
+		self = Self(RAW_staticbuff:&bytes)
+	}
+
+	public init?(netmaskPrefix:UInt8) {
+		guard netmaskPrefix <= 128 else {
+			return nil
+		}
+		var bytes = [UInt8](repeating:0, count:16)
+		let fullBytes = Int(netmaskPrefix / 8)
+		let extraBits = Int(netmaskPrefix % 8)
+		for i in 0..<fullBytes {
+			bytes[i] = 0xFF
+		}
+		if extraBits > 0 {
+			bytes[fullBytes] = 0xFF << (8 - extraBits)
+		}
+		self = Self(RAW_staticbuff:&bytes)
+	}
+
+	public func masked(by netmask:AddressV6) -> AddressV6 {
+		return self.RAW_access_staticbuff { addressBytes -> AddressV6 in
+			netmask.RAW_access_staticbuff { maskBytes -> AddressV6 in
+				let addressPtr = addressBytes.assumingMemoryBound(to:UInt8.self)
+				let maskPtr = maskBytes.assumingMemoryBound(to:UInt8.self)
+				return withUnsafePointer(to:(addressPtr[0] & maskPtr[0], addressPtr[1] & maskPtr[1], addressPtr[2] & maskPtr[2], addressPtr[3] & maskPtr[3], addressPtr[4] & maskPtr[4], addressPtr[5] & maskPtr[5], addressPtr[6] & maskPtr[6], addressPtr[7] & maskPtr[7], addressPtr[8] & maskPtr[8], addressPtr[9] & maskPtr[9], addressPtr[10] & maskPtr[10], addressPtr[11] & maskPtr[11], addressPtr[12] & maskPtr[12], addressPtr[13] & maskPtr[13], addressPtr[14] & maskPtr[14], addressPtr[15] & maskPtr[15])) {
+					return AddressV6(RAW_staticbuff:$0)
+				}
+			}
 		}
 	}
 }
@@ -65,5 +92,64 @@ extension String {
 			let bytes = $0.assumingMemoryBound(to:UInt8.self)
 			return String(format:"%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x", bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7], bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15])
 		})
+	}
+}
+
+public typealias RangeV6 = ClosedRange<AddressV6>
+extension RangeV6 {
+	public init?(_ rangeString:String) {
+		let parts = rangeString.split(separator:"-", omittingEmptySubsequences:true).map(String.init)
+		guard parts.count == 2, let lower = AddressV6(parts[0]), let upper = AddressV6(parts[1]), lower <= upper else {
+			return nil
+		}
+		self = lower...upper
+	}
+
+	public init(address:AddressV6, netmask:AddressV6) {
+		var lowerBytes = [UInt8](repeating:0, count:16)
+		var upperBytes = [UInt8](repeating:0, count:16)
+		withUnsafePointer(to:address) { address in
+			withUnsafePointer(to:netmask) { netmask in
+				for i in 0..<16 {
+					lowerBytes[i] = address.withMemoryRebound(to:UInt8.self, capacity:16) { $0[i] & netmask.withMemoryRebound(to:UInt8.self, capacity:16) { $0[i] } }
+					upperBytes[i] = address.withMemoryRebound(to:UInt8.self, capacity:16) { $0[i] | ~netmask.withMemoryRebound(to:UInt8.self, capacity:16) { $0[i] } }
+				}
+			}
+		}
+		self = AddressV6(RAW_staticbuff:&lowerBytes)...AddressV6(RAW_staticbuff:&upperBytes)
+	}
+}
+
+
+public struct NetworkV6 {
+	public let address:AddressV6
+	public let prefix:UInt8
+	
+	public let range:RangeV6
+	
+	public init?(_ cidr:String) {
+		let parts = cidr.split(separator:"/").map(String.init)
+		guard parts.count == 2,
+			let address = AddressV6(parts[0]),
+			let prefix = UInt8(parts[1]),
+			prefix <= 128 else {
+			return nil
+		}
+		
+		self.address = address
+		self.prefix = prefix
+		let hasNetmaskPrefix = AddressV6(netmaskPrefix:prefix)
+		guard hasNetmaskPrefix != nil else {
+			return nil
+		}
+		self.range = RangeV6(address:address, netmask:hasNetmaskPrefix!)
+	}
+	
+	public func contains(_ address:AddressV6) -> Bool {
+		return range.contains(address)
+	}
+	
+	public func overlaps(with network:NetworkV6) -> Bool {
+		return range.overlaps(network.range)
 	}
 }
