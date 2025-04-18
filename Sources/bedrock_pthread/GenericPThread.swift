59a6d5c8f2e5dbe4d0c213d1b18a9737d10cec8f
tannerdsilva <t@tannersilva.com>
2025-04-18 11:04:05 -0500

    wip bedrock_pthread

---
+++ Sources/bedrock_pthread/GenericPThread.swift
@@ -0,0 +1,33 @@
+/*
+LICENSE MIT
+copyright (c) tanner silva 2025. all rights reserved.
+
+bedrock
+
+*/
+
+/// represents a pthread worker that takes a function as an argument, runs passed work function, and returns the result. if an error is thrown within the work function, it is returned as a failure.
+public struct GenericPThread<R:Sendable>:PThreadWork {
+	/// function to run.
+	private let funcToRun:Argument
+
+	/// the argument type for the function to run.
+	public typealias Argument = @Sendable () throws -> R
+	
+	/// the return type for the function to run.
+	public typealias ReturnType = R
+
+	/// creates a new instance of GenericPThread.
+	/// - parameters:
+	/// 	- argument: the function to run.
+	public init(_ argument:@escaping Argument) {
+		self.funcToRun = argument
+	}
+
+	/// runs the function and returns the result.
+	/// - returns: the result of the function.
+	/// - throws: any error that prevents the work from being completed.
+	public mutating func pthreadWork() throws -> R {
+		return try funcToRun()
+	}
+}
