e3cd31a0fc5189c870ace29b535b075f3f12c896
tannerdsilva <t@tannersilva.com>
2025-06-29 18:51:40 -0500

    improvements to date

--- Sources/bedrock/Date.swift
+++ Sources/bedrock/Date.swift
@@ -36,39 +36,59 @@ internal let encDate = encodingReferenceDate()
 @RAW_staticbuff(bytes:8)
 @RAW_staticbuff_binaryfloatingpoint_type<Double>()
 @frozen public struct Date:Sendable {
-	/// initialize a new date based on the GMT timezone.
+
+	@RAW_staticbuff(bytes:8)
+	@RAW_staticbuff_fixedwidthinteger_type<UInt64>(bigEndian:true)
+	@frozen public struct Seconds:Sendable {
+		public init() {
+			self.init(RAW_native:UInt64(Date().RAW_native()))
+		}
+		public init(localTime:Bool) {
+			self.init(RAW_native:UInt64(Date(localTime:localTime).RAW_native()))
+		}
+		/// returns the difference in time between the called instance and passed date
+		public func timeIntervalSince(_ other:Self) -> UInt64 {
+			return self.RAW_native() - other.RAW_native()
+		}
+		/// returns a new value that is the sum of the current value and the passed interval
+		public func addingTimeInterval(_ interval:UInt64) -> Self {
+			return Self(RAW_native:self.RAW_native() + interval)
+		}
+		/// returns the time interval since Unix date
+		public func timeIntervalSinceUnixDate() -> UInt64 {
+			return self.RAW_native()
+		}
+		/// returns the time interval since the reference date (00:00:00 UTC on 1 January 2001)
+		public func timeIntervalSinceReferenceDate() -> UInt64 {
+			return self.RAW_native() - 978307200
+		}
+	}
+	
+	/// Initialize a new date based on the GMT timezone.
 	public init() {
 		self = Self(localTime:false)
 	}
 
-	/// initialize a new date based on either the local timezone or the GMT timezone.
+	/// Initialize a new date based on either the local timezone or the GMT timezone.
 	public init(localTime:Bool) {
-
 		// capture the current time.
 		var ts = timespec()
 		clock_gettime(CLOCK_REALTIME, &ts)
 		let seconds = Double(ts.tv_sec)
 		let nanoseconds = Double(ts.tv_nsec)
 		let total_time = seconds + (nanoseconds / 1_000_000_000)
-		
-		switch localTime {
-			case false:
-				// capture the local time and account for the timezone adjustments
-				var Cnow = time_t()
-				let loc = localtime(&Cnow).pointee
-				var offset = Double(loc.tm_gmtoff)
-				
-				// correct for daylight savings time if it is in effect
-				if loc.tm_isdst > 0 {
-					offset += 3600
-				}
-				
-				self.init(RAW_native:(total_time + offset))
-			case true:
-				self.init(RAW_native:total_time)
+		if localTime {
+			// apply the local timezone offset
+			var now = time_t(ts.tv_sec)
+			let loc = localtime(&now)!.pointee
+			let offset = Double(loc.tm_gmtoff)
+			self.init(RAW_native:total_time + offset)
+		} else {
+			// keep in UTC
+			self.init(RAW_native:total_time)
 		}
 	}
-
+	
 	/// initialize with a Unix epoch interval (seconds since 00:00:00 UTC on 1 January 1970)
 	public init(unixInterval:Double) {
 		self.init(RAW_native:unixInterval)
