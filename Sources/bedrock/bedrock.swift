514cc8099026ce7de5b3396cc9ffd5055e3019d9
tannerdsilva <t@tannersilva.com>
2025-06-29 22:05:47 -0500

    added path file size extension

--- Sources/bedrock/bedrock.swift
+++ Sources/bedrock/bedrock.swift
@@ -10,3 +10,14 @@ import Darwin
 public func getCurrentUser() -> String {
 	return String(validatingCString:getpwuid(geteuid()).pointee.pw_name)!
 }
+
+extension bedrock.Path {
+	/// returns the size of any file found at the path represented by the bedrock.Path structure.
+	public func getFileSize() -> off_t {
+		var statObj = stat()
+		guard stat(path(), &statObj) == 0 else {
+			return 0
+		}
+		return statObj.st_size
+	}
+}
